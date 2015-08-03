90cb00257ad6023f6cc7c8f328707b4150ae3100
fand <fand@gmork.in>
2015-08-04 00:11:20 +0900

    fixed serve task

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -4,6 +4,7 @@ import pm2 from 'pm2';
 import { pm2 as config } from '../config';
 
 const p = function (method, arg) {
+  console.log(`## serve: gonna ${method}`);
   return new Promise((resolve, reject) => {
     const resolver = (err, res) => {
       if (err) { reject(err); }
@@ -21,8 +22,12 @@ const start          = (app) => p('start', app);
 const gracefulReload = (app) => p('gracefulReload', app);
 
 export function serve () {
-  connect().then(list).then((list) => {
-    if (list && list.length > 0) {
+  return connect().then(list).then((procs) => {
+    if (!procs) { return false; }
+    if (procs.length === 0) { return false; }
+    return procs.some(proc => proc.pm2_env.pm_exec_path === config.app);
+  }).then((isRunning) => {
+    if (isRunning) {
       return gracefulReload(config.app);
     }
     else {
