41a804cf5f073ba1a6ac2e88b471b2d91e470a44
fand <fand@gmork.in>
2015-08-05 02:55:45 +0900

    wrote tmp styles

--- src/js/component/Sampler.js
+++ src/js/component/Sampler.js
@@ -6,6 +6,10 @@ class SamplerVM {
   constructor (sampler) {
     this.sampler = sampler;
   }
+
+  getKitName () {
+    return 'AMEN';
+  }
 }
 
 export default {
@@ -16,23 +20,26 @@ export default {
   view : function (vm) {
     return m('.Sampler', [
       m('.Sampler__Header', [
-        m('.Sampler__KitName', 'mykit'),
+        m('.Sampler__KitName', 'Drumkit'),
         m('select.Sampler__KitSelector', [
           m('option', { value: 'AMEN', selected: 'selected' }, 'Amen')
         ]),
       ]),
       m('.Sampler__Body', [
         m('.Sampler__Red', [
-          m('.Sampler__SampleName', 'Kick'),
-          m('.Sampler__Play', {}),
+          m('.Sampler__SampleNameLabel', 'Red'),
+          m('.Sampler__SampleName', 'Kick.wav'),
+          m('.Sampler__Play', '➤'),
         ]),
         m('.Sampler__Green', [
-          m('.Sampler__SampleName', 'Snare'),
-          m('.Sampler__Play', {}),
+          m('.Sampler__SampleNameLabel', 'Green'),
+          m('.Sampler__SampleName', 'Snare.wav'),
+          m('.Sampler__Play', '➤'),
         ]),
         m('.Sampler__Blue', [
-          m('.Sampler__SampleName', 'Crash'),
-          m('.Sampler__Play', {}),
+          m('.Sampler__SampleNameLabel', 'Blue'),
+          m('.Sampler__SampleName', 'Crash.wav'),
+          m('.Sampler__Play', '➤'),
         ]),
       ]),
     ]);
