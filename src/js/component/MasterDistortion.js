0dea0b0cbeb969b70060bc830b3e50a361b2c860
fand <fand@gmork.in>
2015-08-12 21:58:11 +0900

    remove Distortion component

--- src/js/component/MasterDistortion.js
+++ src/js/component/MasterDistortion.js
@@ -1,9 +1,30 @@
 'use strict';
 
 import m from 'mithril';
-import DistortionVM from './Distortion/VM';
 
-class MasterDistortionVM extends DistortionVM {
+class MasterDistortionVM {
+
+  constructor (node) {
+    this.node = node;
+
+    this.distortion = m.prop(10000);
+    this.volume     = m.prop(3000);
+  }
+
+  onChangeDistortion (e) {
+    this.distortion(e.target.value);
+    this.node.setDistortion(e.target.value / 10000.0);
+  }
+
+  onChangeVolume (e) {
+    this.volume(e.target.value);
+    this.node.setVolume(e.target.value / 10000.0);
+  }
+
+  toggle () {
+    this.node.toggle();
+  }
+
 }
 
 export default {
