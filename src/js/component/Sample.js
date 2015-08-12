d9afc76c8013be065924a7fe150cebac481533f4
fand <fand@gmork.in>
2015-08-12 21:42:17 +0900

    move input sliders to Sample component

--- src/js/component/Sample.js
+++ src/js/component/Sample.js
@@ -8,31 +8,42 @@ const CANVAS_HEIGHT = 256;
 
 class SampleVM {
   constructor (args) {
-    this.sample     = args.sample;
-    this.distortion = args.distortion;
-    this.index      = args.index;
-    this.callback   = args.callback;
-    this.colorLabel = args.colorLabel;
-    this.color      = args.color;
+    this.sampleNode     = args.sample;
+    this.distortionNode = args.distortion;
+    this.index          = args.index;
+    this.callback       = args.callback;
+    this.colorLabel     = args.colorLabel;
+    this.color          = args.color;
+
+    this.distortion = m.prop(10000);
+    this.volume     = m.prop(3000);
   }
 
   onClickPlayButton () {
-    this.sample.play();
+    this.sampleNode.play();
   }
 
   getSampleName () {
-    return this.sample.basename;
+    return this.sampleNode.basename;
+  }
+
+  onChangeDistortion (e) {
+    this.distortion(e.target.value);
+    this.distortionNode.setDistortion(e.target.value / 10000.0);
+  }
+
+  onChangeVolume (e) {
+    this.volume(e.target.value);
+    this.distortionNode.setVolume(e.target.value / 10000.0);
   }
 
   drawWave (element, isInitialized, context) {
     if (isInitialized) { return; }
 
-    this.sample.getWaveData().then((wave) => {
+    this.sampleNode.getWaveData().then((wave) => {
       var ctx = element.getContext("2d");
       const rect = element.getBoundingClientRect();
       const [w, h] = [rect.width, rect.height];
-      // const [w, h] = [CANVAS_WIDTH, CANVAS_HEIGHT];
-      console.log([w, h]);
 
       ctx.lineWidth = 2
 
@@ -60,20 +71,39 @@ export default {
   },
 
   view : function (vm) {
-    return m('.Sampler__Sample', {
+    return m('.Sample', {
       class : vm.colorLabel,
     }, [
-      m('.Sampler__Sample__NameLabel', vm.colorLabel),
-      m('.Sampler__Sample__Name', vm.getSampleName()),
-      m('.Sampler__Sample__Play', {
+      m('.Sample__NameLabel', vm.colorLabel),
+      m('.Sample__Name', vm.getSampleName()),
+      m('.Sample__Play', {
         onclick : ::vm.onClickPlayButton,
       }, '➤'),
-      m('canvas.Sampler__Sample__Wave', {
+      m('canvas.Sample__Wave', {
         config: ::vm.drawWave,
       }),
-      m.component(Distortion, {
-        distortionNode : vm.distortion,
-      }),
+      m('.Sample__FXs', [
+        m('.Sample__FXs__FX', [
+          m('.Sample__FXs__FX__Label', 'distortion'),
+          m('input.Sample__FXs__FX__Input', {
+            type     : 'range',
+            min      : 10000,
+            max      : 30000,
+            onchange : ::vm.onChangeDistortion,
+            value    : vm.distortion()
+          }),
+        ]),
+        m('.Sample__FXs__FX', [
+          m('.Sample__FXs__FX__Label', 'volume'),
+          m('input.Sample__FXs__FX__Input', {
+            type     : 'range',
+            min      : 0,
+            max      : 10000,
+            onchange : ::vm.onChangeVolume,
+            value    : vm.volume()
+          }),
+        ]),
+      ]),
     ]);
   },
 
