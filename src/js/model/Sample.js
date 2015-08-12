717fe5142f6bf3d3c6847fb6a3798dfa5118bd4a
fand <fand@gmork.in>
2015-08-12 22:05:53 +0900

    add pitch input

--- src/js/model/Sample.js
+++ src/js/model/Sample.js
@@ -6,6 +6,8 @@ class Sample extends Node {
 
   constructor (url) {
     super();
+    this.playbackRate = 1.0;
+
     this.isLoaded = this.loadSample(url)
       .then(buffer => this.buffer = buffer);
   }
@@ -14,6 +16,7 @@ class Sample extends Node {
     if (this.node) { this.node.stop(0); }
     this.node = this.ctx.createBufferSource();
     this.node.buffer = this.buffer;
+    this.node.playbackRate.value = this.playbackRate;
     this.node.connect(this.wet);
     this.node.start(0);
   }
@@ -43,6 +46,10 @@ class Sample extends Node {
     return this.isLoaded.then(() => {
       return this.buffer.getChannelData(0);
     });
+  }
+
+  setPlaybackRate (playbackRate) {
+    this.playbackRate = playbackRate;
   }
 
 }
